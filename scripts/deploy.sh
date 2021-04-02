de741f570f1843b45f6fa00c0e143e4fd833e230
MattheusAlves <matheus_alves0101@outlook.com>
2021-04-02 16:53:48 -0300

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
+
+#!/usr/bin/env bash
+
+# Init .ssh dir and expand $SSH_KEY
 mkdir -p ~/.ssh
-echo -e "${SSH_KEY}}//_/\\n" > ~/.ssh/id_rsa
+echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/know_hosts 
